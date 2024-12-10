93540757a359e470838db91289fea3a01acddc53
H4ND4 <165560485+H4ND4@users.noreply.github.com>
2024-12-11 01:40:29 +0800

    Add files via upload

---
+++ movie.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+# Title of the page
+st.markdown("<h1 style='text-align: center;'>Welcome to the Movie App 🎬</h1>", unsafe_allow_html=True)
+
+# Home Page content
+st.write("""
+Welcome to the **Movie App**! This is your one-stop destination to explore, search, and discover the best movies from various genres and regions.
+
+### Explore Movies
+You can dive into a wide variety of movies by exploring:
+- **Trending Movies**: Check out the latest movies that are trending worldwide.
+- **Top Rated**: Discover the highest-rated movies of all time or filter by release year.
+- **Recently Released**: Stay updated with the most recent movie releases.
+- **Movies by Genre**: Choose from various genres like Action, Drama, Comedy, and more to find movies tailored to your preferences.
+
+### Movie Search
+You can search for any movie by title and get detailed information, including:
+- Movie plot
+- Cast and crew
+- Ratings (IMDb, Rotten Tomatoes)
+- Box office earnings
+- And much more!
+""")
+
+# EDA Analysis Section
+st.subheader("EDA Analysis")
+
+image_path = r"C:\Users\handd\OneDrive\Desktop\Intermediate Python\GitHub\intermediate-python-fall-2024\Practices\Project2\download.png"
+st.image(image_path, caption='Popularity by Genre (Average IMDb Rating by Genre)', use_column_width=True)
+
+image_path = r"C:\Users\handd\OneDrive\Desktop\Intermediate Python\GitHub\intermediate-python-fall-2024\Practices\Project2\2.png"
+st.image(image_path, caption='Top Directors by Movie Count', use_column_width=True)
+
+image_path = r"C:\Users\handd\OneDrive\Desktop\Intermediate Python\GitHub\intermediate-python-fall-2024\Practices\Project2\3.png"
+st.image(image_path, caption='Top 10 Movies by IMDb Rating', use_column_width=True)
